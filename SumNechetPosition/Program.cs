3fa9a40341d929fe019af08d94304227e05642af
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-12-08 23:13:31 +0300

    add SumNechetPos

---
+++ SumNechetPosition/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int[] mas = new int[10];
+Random rnd = new Random();
+Console.WriteLine($"Массив:");
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = rnd.Next(-15, 15);
+    Console.Write(mas[i] + " ");
+}
+int sum = FindSum(mas);
+Console.WriteLine($"\nСумма чисел на нечетных позициях (начиная отсчет с 0): {sum} ");
+
+
+int FindSum(int[] mas)
+{
+    int sum = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum += mas[i];
+        }
+    }
+    return sum;
+}
